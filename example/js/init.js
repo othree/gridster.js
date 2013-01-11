411e0129742d4d30112603788a28b9a05102b454
othree_kao <othree_kao@htc.com>
2013-01-11 11:13:22 +0800

    Update style, add source map

--- example/js/init.js
+++ example/js/init.js
@@ -1,24 +1,25 @@
+/*global $: false, document: false */
+/*jslint plusplus: true */
 (function () {
+    "use strict";
 	var grid = $(".gridster > ul").gridster({
 	    widget_margins: [10, 10],
 	    widget_base_dimensions: [140, 140],
-	    min_cols: 6,
-	    extra_cols: 0
-	}).data('gridster');
+	    min_cols: 6
+    }).data('gridster'),
 
-	var add_block = function () {
-		size_x = Math.floor(Math.random() * 2, 0) + 1;
-		size_y = Math.floor(Math.random() * 2, 0) + 1;
+        add_block = function () {
+            var size_x = Math.floor(Math.random() * 2, 0) + 1,
+                size_y = Math.floor(Math.random() * 2, 0) + 1;
 
-		grid.add_widget('<li></li>', size_x, size_y);
-	};
+            grid.add_widget('<li></li>', size_x, size_y);
+        };
 
 	$('#add').click(function () {
 		var count = 5;
 		while (--count) {
 			add_block();
 		}
-		$('body').animate({scrollTop: document.body.scrollHeight}, 'slow');
+		$('body').stop().animate({scrollTop: document.body.scrollHeight}, 'slow');
 	});
-}())
-
+}());
